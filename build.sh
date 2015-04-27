ae5f8f0c117f60f3d38deb9feabcf4c4aadd474d
Allycen <ponmike92@yandex.ru>
2015-04-27 12:43:01 +0300

    first commit

---
+++ build.sh
@@ -0,0 +1,37 @@
+# pre clean
+
+
+#  gather songs
+cat ./songs/отрядные/*.tex > OTRYAD_SONGS.txt 
+cat ./songs/сачок/*.tex > SOCKO_SONGS.txt 
+cat ./songs/лагерные/*.tex > CAMP_SONGS.txt 
+cat ./songs/всеми\ любимые/*.tex > LOVE_SONGS.txt 
+
+
+#  gen pdf
+lilypond-book --pdf book.lytex
+pdflatex book.tex
+
+
+# gen index-files
+songidx titlfile1.sxd titlfile1.sbx
+songidx authfile1.sxd authfile1.sbx
+
+songidx titlfile2.sxd titlfile2.sbx
+songidx authfile2.sxd authfile2.sbx
+
+songidx titlfile3.sxd titlfile3.sbx
+songidx authfile3.sxd authfile3.sbx
+
+songidx titlfile4.sxd titlfile4.sbx
+songidx authfile4.sxd authfile4.sbx
+
+
+
+lilypond-book --pdf book.lytex
+pdflatex book.tex
+
+
+
+shopt -s extglob
+rm -rf !(book.lytex|build.sh|songs|img|book.pdf)
