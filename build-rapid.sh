7ee61eb178ef155936d37e1bbaf87d5cba58e5ca
Allycen <ponmike92@yandex.ru>
2015-05-02 03:54:25 +0300

    add build-rapid.sh (for faster PDF views) fix text geometry

---
+++ build-rapid.sh
@@ -0,0 +1,19 @@
+
+#  gather songs
+cat ./songs/отрядные/*.tex > OTRYAD_SONGS.txt 
+cat ./songs/сачок/*.tex > SOCKO_SONGS.txt 
+cat ./songs/лагерные/*.tex > CAMP_SONGS.txt 
+cat ./songs/всеми\ любимые/*.tex > LOVE_SONGS.txt 
+
+
+#  gen pdf
+lilypond-book --pdf book.lytex
+pdflatex book.tex
+
+
+
+
+# clean
+shopt -s extglob
+rm -rf !(book.lytex|build.sh|songs|img|book.pdf|conv.sh|conv|build-rapid.sh)
+
